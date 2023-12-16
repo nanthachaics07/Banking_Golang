8e407fa94f8806db3fd66484ec7d8e972fd312fc
Nanthachai.CS07 <botsafe002@gmail.com>
2023-12-16 20:32:12 +0700

    add go main

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"bank_test01/repository"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
+)
+
+func main() {
+	db, err := sqlx.Open("mysql", "root:babana@(localhost:3306)/banking?parseTime=true")
+	if err != nil {
+		panic(err)
+	}
+
+	customerRepository := repository.NewCustomerRepositoryDB(db)
+	_ = customerRepository
+
+	customers, err := customerRepository.GetAll()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(customers)
+}
