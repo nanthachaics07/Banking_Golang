589edf515a8d9954319058a760885dbaafff2cc7
Nanthachai.CS07 <botsafe002@gmail.com>
2023-12-17 16:38:44 +0700

    Add check Viper err

--- main.go
+++ main.go
@@ -49,4 +49,11 @@ func initConfig() {
 	viper.SetConfigName("config")
 	viper.SetConfigType("yaml")
 	viper.AddConfigPath(".")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+
+	}
+
 }
